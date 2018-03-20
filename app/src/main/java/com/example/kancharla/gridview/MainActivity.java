10a58bb031551127fb3ee1eae49c3d1be1f6260a
yaswanthzzz <yaswanth.kancharla15@gmail.com>
2018-03-20 10:15:22 +0530

    text

---
+++ app/src/main/java/com/example/kancharla/gridview/MainActivity.java
@@ -0,0 +1,35 @@
+package com.example.kancharla.gridview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.GridView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+  static final  String[] alphabets=new String[]{"a","b","c","d","e","f","g","h","i"};
+
+    GridView gridview;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ArrayAdapter adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,alphabets);
+
+         gridview=(GridView) findViewById(R.id.gridview);
+
+
+        gridview.setAdapter(adapter);
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
+                Toast.makeText(getApplicationContext(), ((TextView) v).getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }}
